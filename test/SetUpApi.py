41ab3ae5bd5c523bed18deb5776616430c7615dc
j5465 <41243685+j5465@users.noreply.github>
2024-01-03 22:58:42 +0800

    1. pip依赖 2. 初始化密钥

--- test/SetUpApi.py
+++ test/SetUpApi.py
@@ -1,9 +1,22 @@
 from okx import Trade
 from loadEnv import load_env_tuple
+from okx.websocket.WsPublicAsync import WsPublicAsync
+from okx.websocket.WsPprivateAsync import WsPrivateAsync
 
 
 class SetUpApi:
     def __init__(self, isDemo="0"):
+        self.api_key, self.api_secret_key, self.passphrase = load_env_tuple()
+        print("SetUpApi api_key: " + self.api_key)
+        self.tradeApi = Trade.TradeAPI(self.api_key, self.api_secret_key, self.passphrase, False, isDemo)
+
+class SetUpWs:
+    def __init__(self, url):
         api_key, api_secret_key, passphrase = load_env_tuple()
-        print("SetUpApi api_key: " + api_key)
-        self.tradeApi = Trade.TradeAPI(api_key, api_secret_key, passphrase, False, isDemo)
+        self.ws = WsPrivateAsync(
+            apiKey=api_key,
+            passphrase=passphrase,
+            secretKey=api_secret_key,
+            url=url,
+            useServerTime=False
+        )
